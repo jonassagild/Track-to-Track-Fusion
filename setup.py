df126899542c35d334bfbc32d681c92191b10548
Jonas Åsnes Sagild <jonassagild@hotmail.com>
2020-08-25 08:32:19 +0200

    add setup file

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='Track-to-Track-Fusion',
+    version='0.1dev',
+    packages=['scripts',],
+    license='TODO',
+    long_description=open('README.md').read()
+)
