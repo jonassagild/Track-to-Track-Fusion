83a06fca23842c0458a39d1ac316e2047b514acf
Jonas Åsnes Sagild <jonassagild@hotmail.com>
2020-08-28 11:15:40 +0200

    implements the fusion under the error independence assumption

--- data_fusion/track_to_track_fusion.py
+++ data_fusion/track_to_track_fusion.py
@@ -1,13 +1,31 @@
 """track_to_track_fusion
 
+The module fuses two tracks under the error independence assumption. See the report for derivations.
 todo
 """
+import numpy as np
+from stonesoup.types.update import GaussianStateUpdate
+
 
 def fuse(track1, track2):
     """
-    fuse the tracks todo
+    fuses the tracks todo
     :param track1:
     :param track2:
     :return:
     """
-    pass
+    P_j = track1.covar  # covar sensor j
+    P_i = track2.covar  # covar sensor i
+    x_j = track1.state_vector  # posterior sensor j
+    x_i = track2.state_vector  # posterior sensor i
+
+    # fusion equation under the error independence assumption
+    fused_posterior = P_j @ np.linalg.inv(P_i + P_j) @ x_i + P_i @ np.linalg.inv(P_i + P_j) @ x_j
+
+    # covariance fusion equation
+    fused_covar = P_i @ np.linalg.inv(P_i + P_j) @ P_j
+
+    # store fused information in GaussianStateUpdate object
+    # fused = GaussianStateUpdate(fused_posterior, fused_covar)
+    # todo return an object and not only the posterior and covar
+    return fused_posterior, fused_covar
