9e16d978f9f1a18fb21860a5d8ddb20bafa01391
Jonas Åsnes Sagild <jonassagild@hotmail.com>
2020-09-10 14:57:20 +0200

    fix comments

--- trackers/calc_cross_cov_estimate_error.py
+++ trackers/calc_cross_cov_estimate_error.py
@@ -6,22 +6,22 @@ See report to find derivation and formula for calculating the cross-covariance o
 import numpy as np
 
 
-def calc_cross_cov_estimate_error(h_i, h_j, kalman_gain_i, kalman_gain_j, f_i, f_j, q, prev_cross_cov):
+def calc_cross_cov_estimate_error(h_i, h_j, kalman_gain_i, kalman_gain_j, f, q, prev_cross_cov):
     """
-    Calculates the cross-covariance of the estimation error. See report for description of variables and formula
+    Calculates the cross-covariance of the estimation error. See report for description of variables and formula.
+    Assumes same transition model for both trackers
     :param prev_cross_cov:
     :param kalman_gain_j:
     :param kalman_gain_i:
     :param h_i:
     :param h_j: 
-    :param f_i:
-    :param f_j: 
+    :param f: assumes same transition models for both trackers
     :param q: 
     :return:
     """
     # TODO needs refactoring when decided whether to use semantics or mathematical characters. (uses both currently)
-    cross_cov = (np.eye(prev_cross_cov.shape) - kalman_gain_i @ h_i) @ np.linalg.inv(f_i @ prev_cross_cov @ f_i.T + q) \
-                @ (np.eye(prev_cross_cov.shape) - kalman_gain_j @ h_j).T
+    cross_cov = (np.eye(prev_cross_cov.shape[0]) - kalman_gain_i @ h_i) @ np.linalg.inv(f @ prev_cross_cov @ f.T + q) \
+                @ (np.eye(prev_cross_cov.shape[0]) - kalman_gain_j @ h_j).T
     return cross_cov
 
 
