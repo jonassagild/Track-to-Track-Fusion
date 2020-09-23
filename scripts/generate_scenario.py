4cdb3d2642587c2967bc5f02194a8248d32b040d
Jonas Åsnes Sagild <jonassagild@hotmail.com>
2020-09-23 16:24:03 +0200

    change script to function

--- scripts/generate_scenario.py
+++ scripts/generate_scenario.py
@@ -19,62 +19,64 @@ from stonesoup.models.measurement.linear import LinearGaussian
 
 from utils import store_object
 
-start_time = datetime.now()
 
-# specify seed to be able repeat example
-np.random.seed(1996)
+def generate_scenario(seed=1996):
+    # specify seed to be able repeat example
+    start_time = datetime.now()
 
-# combine two 1-D CV models to create a 2-D CV model
-transition_model = CombinedLinearGaussianTransitionModel([ConstantVelocity(0.01), ConstantVelocity(0.01)])
+    np.random.seed(seed)
 
-# starting at 0,0 and moving NE
-truth = GroundTruthPath([GroundTruthState([0, 1, 0, 1], timestamp=start_time)])
+    # combine two 1-D CV models to create a 2-D CV model
+    transition_model = CombinedLinearGaussianTransitionModel([ConstantVelocity(0.01), ConstantVelocity(0.01)])
 
-# generate truth using transition_model and noise
-for k in range(1, 21):
-    truth.append(GroundTruthState(
-        transition_model.function(truth[k - 1], noise=True, time_interval=timedelta(seconds=1)),
-        timestamp=start_time + timedelta(seconds=k)))
+    # starting at 0,0 and moving NE
+    truth = GroundTruthPath([GroundTruthState([0, 1, 0, 1], timestamp=start_time)])
 
-# Simulate measurements
-# Specify measurement model for radar
-measurement_model_radar = LinearGaussian(
-    ndim_state=4,  # number of state dimensions
-    mapping=(0, 2),  # mapping measurement vector index to state index
-    noise_covar=np.array([[3, 0],  # covariance matrix for Gaussian PDF
-                          [0, 3]])
-)
+    # generate truth using transition_model and noise
+    for k in range(1, 21):
+        truth.append(GroundTruthState(
+            transition_model.function(truth[k - 1], noise=True, time_interval=timedelta(seconds=1)),
+            timestamp=start_time + timedelta(seconds=k)))
 
-# Specify measurement model for AIS
-measurement_model_ais = LinearGaussian(
-    ndim_state=4,
-    mapping=(0, 2),
-    noise_covar=np.array([[1, 0],
-                          [0, 1]])
-)
+    # Simulate measurements
+    # Specify measurement model for radar
+    measurement_model_radar = LinearGaussian(
+        ndim_state=4,  # number of state dimensions
+        mapping=(0, 2),  # mapping measurement vector index to state index
+        noise_covar=np.array([[3, 0],  # covariance matrix for Gaussian PDF
+                              [0, 3]])
+    )
 
-# generate "radar" measurements
-measurements_radar = []
-for state in truth:
-    measurement = measurement_model_radar.function(state, noise=True)
-    measurements_radar.append(Detection(measurement, timestamp=state.timestamp))
+    # Specify measurement model for AIS
+    measurement_model_ais = LinearGaussian(
+        ndim_state=4,
+        mapping=(0, 2),
+        noise_covar=np.array([[1, 0],
+                              [0, 1]])
+    )
 
-# generate "AIS" measurements
-measurements_AIS = []
-state_num = 0
-for state in truth:
-    state_num += 1
-    if not state_num % 2:  # measurement every second timestep
-        measurement = measurement_model_ais.function(state, noise=True)
-        measurements_AIS.append(Detection(measurement, timestamp=state.timestamp))
+    # generate "radar" measurements
+    measurements_radar = []
+    for state in truth:
+        measurement = measurement_model_radar.function(state, noise=True)
+        measurements_radar.append(Detection(measurement, timestamp=state.timestamp))
 
-# save the ground truth and the measurements for the radar and the AIS
-store_object.store_object(truth, "../scenarios/scenario1/ground_truth.pk1")
-store_object.store_object(measurements_radar, "../scenarios/scenario1/measurements_radar.pk1")
-store_object.store_object(measurements_AIS, "../scenarios/scenario1/measurements_ais.pk1")
-store_object.store_object(start_time, "../scenarios/scenario1/start_time.pk1")
-store_object.store_object(measurement_model_radar, "../scenarios/scenario1/measurement_model_radar.pk1")
-store_object.store_object(measurement_model_ais, "../scenarios/scenario1/measurement_model_ais.pk1")
-store_object.store_object(transition_model, "../scenarios/scenario1/transition_model.pk1")
+    # generate "AIS" measurements
+    measurements_ais = []
+    state_num = 0
+    for state in truth:
+        state_num += 1
+        if not state_num % 2:  # measurement every second timestep
+            measurement = measurement_model_ais.function(state, noise=True)
+            measurements_ais.append(Detection(measurement, timestamp=state.timestamp))
+
+    # save the ground truth and the measurements for the radar and the AIS
+    store_object.store_object(truth, "../scenarios/scenario1/ground_truth.pk1")
+    store_object.store_object(measurements_radar, "../scenarios/scenario1/measurements_radar.pk1")
+    store_object.store_object(measurements_ais, "../scenarios/scenario1/measurements_ais.pk1")
+    store_object.store_object(start_time, "../scenarios/scenario1/start_time.pk1")
+    store_object.store_object(measurement_model_radar, "../scenarios/scenario1/measurement_model_radar.pk1")
+    store_object.store_object(measurement_model_ais, "../scenarios/scenario1/measurement_model_ais.pk1")
+    store_object.store_object(transition_model, "../scenarios/scenario1/transition_model.pk1")
 
 
