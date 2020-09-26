424b28d26f01b011d1bc773a469194b9dd523b72
Jonas Åsnes Sagild <jonassagild@hotmail.com>
2020-09-26 14:43:36 +0200

    fix intial conditions and add plotting

--- trackers/kalman_filter_dependent_fusion.py
+++ trackers/kalman_filter_dependent_fusion.py
@@ -121,13 +121,14 @@ for measurement in measurements_ais:
 
 cross_cov_ij = []
 cross_cov_ji = []
-cross_cov_ji.append(np.zeros(4))
-cross_cov_ij.append(np.zeros(4))
+cross_cov_ji.append(np.zeros([4, 4]))
+cross_cov_ij.append(np.zeros([4, 4]))
 
 # TODO change flow to assume that the indexes decide whether its from the same iterations
 # use indexes to loop through tracks, kf_gains etc
 
 tracks_fused = []
+# tracks_fused.append(tracks_radar[0])
 for i in range(1, len(tracks_radar)):
     # we assume that the indexes correlates with the timestamps. I.e. that the lists are 'synchronized'
     # check to make sure
@@ -141,17 +142,18 @@ for i in range(1, len(tracks_radar)):
             measurement_model_radar.matrix(), measurement_model_ais.matrix(), kf_gains_radar[i], kf_gains_ais[i],
             transition_matrixes_radar[i], transition_covars_ais[i], cross_cov_ij[i-1]
         ))
-        # TODO check whether these are the same, just transposed
+        # TODO check whether these are the same, just transposed, aka equal (cov matrice is symmetric)
 
         # test for track association
-        same_target = track_to_track_association.test_association_dependent_tracks(tracks_radar[i], tracks_ais[i],
-                                                                                   cross_cov_ij[i],
-                                                                                   cross_cov_ji[i], 0.01)
+        # same_target = track_to_track_association.test_association_dependent_tracks(tracks_radar[i], tracks_ais[i],
+        #                                                                            cross_cov_ij[i],
+        #                                                                            cross_cov_ji[i], 0.01)
+        same_target = True  # ignore test for track association for now
         if same_target:
             fused_posterior, fused_covar = track_to_track_fusion.fuse_dependent_tracks(tracks_radar[i], tracks_ais[
                 i], cross_cov_ij[i], cross_cov_ji[i])
             tracks_fused.append((fused_posterior, fused_covar))
-            break
+
 
 # plot
 fig = plt.figure(figsize=(10, 6))
@@ -233,6 +235,117 @@ ellipse = Ellipse(xy=(0, 0),
 ax.add_patch(ellipse)
 
 ax.legend()
-ax.set_title("Kalman filter tracking and fusion under the error independence assumption")
+ax.set_title("Kalman filter tracking and fusion accounting for the dependence")
 fig.show()
-fig.savefig("../results/scenario2/KF_tracking_and_fusion_under_error_independence_assumption.svg")
+# fig.savefig("../results/scenario2/KF_tracking_and_fusion_accounting_for_dependence.svg")
+
+# plot estimate for estimate
+# plot
+fig_2 = plt.figure(figsize=(10, 6))
+ax = fig_2.add_subplot(1, 1, 1)
+ax.set_xlabel("$x$")
+ax.set_ylabel("$y$")
+ax.axis('equal')
+ax.plot([state.state_vector[0] for state in ground_truth],
+        [state.state_vector[2] for state in ground_truth],
+        linestyle="--",
+        label='Ground truth')
+# ax.scatter([state.state_vector[0] for state in measurements_radar],
+#            [state.state_vector[1] for state in measurements_radar],
+#            color='b',
+#            label='Measurements Radar')
+# ax.scatter([state.state_vector[0] for state in measurements_ais],
+#            [state.state_vector[1] for state in measurements_ais],
+#            color='r',
+#            label='Measurements AIS')
+
+for i in range(0, len(tracks_fused)):
+    # plot measurements
+    ax.scatter([measurements_radar[i+1].state_vector[0]],
+               [measurements_radar[i+1].state_vector[1]],
+               color='b',
+               label='Measurements Radar')
+    ax.scatter([measurements_ais[i+1].state_vector[0]],
+               [measurements_ais[i+1].state_vector[1]],
+               color='r',
+               label='Measurements AIS')
+
+    # plot one and one estimate
+    state_radar = tracks_radar[i + 1]
+    w, v = np.linalg.eig(measurement_model_radar.matrix() @ state_radar.covar @ measurement_model_radar.matrix().T)
+    max_ind = np.argmax(w)
+    min_ind = np.argmin(w)
+    orient = np.arctan2(v[1, max_ind], v[0, max_ind])
+    ellipse = Ellipse(xy=(state_radar.state_vector[0], state_radar.state_vector[2]),
+                      width=2 * np.sqrt(w[max_ind]), height=2 * np.sqrt(w[min_ind]),
+                      angle=np.rad2deg(orient),
+                      alpha=0.2,
+                      color='b')
+    ax.add_artist(ellipse)
+
+    state_ais = tracks_ais[i + 1]
+    w, v = np.linalg.eig(measurement_model_ais.matrix() @ state_ais.covar @ measurement_model_ais.matrix().T)
+    max_ind = np.argmax(w)
+    min_ind = np.argmin(w)
+    orient = np.arctan2(v[1, max_ind], v[0, max_ind])
+    ellipse = Ellipse(xy=(state_ais.state_vector[0], state_ais.state_vector[2]),
+                      width=2 * np.sqrt(w[max_ind]), height=2 * np.sqrt(w[min_ind]),
+                      angle=np.rad2deg(orient),
+                      alpha=0.2,
+                      color='r')
+    ax.add_patch(ellipse)
+
+    state_fused = tracks_fused[i]
+    w, v = np.linalg.eig(measurement_model_ais.matrix() @ state_fused[1] @ measurement_model_ais.matrix().T)
+    max_ind = np.argmax(w)
+    min_ind = np.argmin(w)
+    orient = np.arctan2(v[1, max_ind], v[0, max_ind])
+    ellipse = Ellipse(xy=(state_fused[0][0], state_fused[0][2]),
+                      width=2 * np.sqrt(w[max_ind]), height=2 * np.sqrt(w[min_ind]),
+                      angle=np.rad2deg(orient),
+                      alpha=0.5,
+                      color='green')
+    ax.add_patch(ellipse)
+
+    fig_2.show()
+    input("Press Enter to continue...")
+
+#
+# # add ellipses to the posteriors
+# for state in tracks_radar:
+#     w, v = np.linalg.eig(measurement_model_radar.matrix() @ state.covar @ measurement_model_radar.matrix().T)
+#     max_ind = np.argmax(w)
+#     min_ind = np.argmin(w)
+#     orient = np.arctan2(v[1, max_ind], v[0, max_ind])
+#     ellipse = Ellipse(xy=(state.state_vector[0], state.state_vector[2]),
+#                       width=2 * np.sqrt(w[max_ind]), height=2 * np.sqrt(w[min_ind]),
+#                       angle=np.rad2deg(orient),
+#                       alpha=0.2,
+#                       color='b')
+#     ax.add_artist(ellipse)
+#
+# for state in tracks_ais:
+#     w, v = np.linalg.eig(measurement_model_ais.matrix() @ state.covar @ measurement_model_ais.matrix().T)
+#     max_ind = np.argmax(w)
+#     min_ind = np.argmin(w)
+#     orient = np.arctan2(v[1, max_ind], v[0, max_ind])
+#     ellipse = Ellipse(xy=(state.state_vector[0], state.state_vector[2]),
+#                       width=2 * np.sqrt(w[max_ind]), height=2 * np.sqrt(w[min_ind]),
+#                       angle=np.rad2deg(orient),
+#                       alpha=0.2,
+#                       color='r')
+#     ax.add_patch(ellipse)
+#
+# for track_fused in tracks_fused:
+#     w, v = np.linalg.eig(measurement_model_ais.matrix() @ track_fused[1] @ measurement_model_ais.matrix().T)
+#     max_ind = np.argmax(w)
+#     min_ind = np.argmin(w)
+#     orient = np.arctan2(v[1, max_ind], v[0, max_ind])
+#     ellipse = Ellipse(xy=(track_fused[0][0], track_fused[0][2]),
+#                       width=2 * np.sqrt(w[max_ind]), height=2 * np.sqrt(w[min_ind]),
+#                       angle=np.rad2deg(orient),
+#                       alpha=0.5,
+#                       color='green')
+#     ax.add_patch(ellipse)
+
+fig_2.show()
