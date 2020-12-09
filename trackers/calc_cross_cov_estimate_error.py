7dbf8a88717cd2a40c5a4e24acac560638202102
Jonas Åsnes  Sagild <jonassagild@hotmail.com>
2020-12-09 19:57:40 +0100

    add calc of cross covar with T2TFwoMp

--- trackers/calc_cross_cov_estimate_error.py
+++ trackers/calc_cross_cov_estimate_error.py
@@ -25,3 +25,14 @@ def calc_cross_cov_estimate_error(h_i, h_j, kalman_gain_i, kalman_gain_j, f, q, 
     return cross_cov
 
 
+def calc_partial_feedback_cross_cov(track1, track2, cross_covar_ij, cross_covar_ji):
+    """
+    Calculates the updated cross_covariance when the partial feedback is used
+    """
+    P_i = track1.covar
+    P_j = track2.covar
+    P_ij = cross_covar_ij
+    P_ji = cross_covar_ji
+    K_12 = (P_i + P_ij) @ np.linalg.inv(P_i + P_j - P_ij - P_ji)
+    cross_covar = (np.eye(4) - K_12) @ cross_covar_ij + K_12 @ P_j
+    return cross_covar
