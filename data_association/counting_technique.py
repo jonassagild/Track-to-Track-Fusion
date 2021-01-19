4e1e30975f8a7a906ab4eb7eb03c02e109af3167
Jonas Åsnes  Sagild <jonassagild@hotmail.com>
2021-01-19 14:35:58 +0100

    add data association module, add skeleton code

---
+++ data_association/counting_technique.py
@@ -0,0 +1,12 @@
+"""
+
+"""
+
+
+def associate_tracks(tracks_1, tracks_2, association_distance_threshold=10, consecutive_hits_confirm_association=3,
+                     consecutive_misses_end_association=2):
+    """
+
+    """
+
+    pass
