3c57224a1bc2f20cce572b4a10cf4448133b0365
Jonas Åsnes  Sagild <jonassagild@hotmail.com>
2021-01-23 14:04:43 +0100

    add Associator interface and start implementing CountingAssociator class

---
+++ data_association/Associator.py
@@ -0,0 +1,11 @@
+
+
+class Associator:
+    """
+    Interface for Associator classes
+    """
+    def __init__(self):
+        pass
+
+    def associate_tracks(self, track1, track2):
+        pass
---
+++ data_association/CountingAssociator.py
@@ -0,0 +1,8 @@
+from data_association import Associator
+
+
+class CountingAssociator(Associator):
+    """
+    Class for the CountingAssociator
+    """
+    pass
