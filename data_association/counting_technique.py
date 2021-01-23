ea6185059cb05f608a12cd32ac7e3f3671badeb1
Jonas Åsnes  Sagild <jonassagild@hotmail.com>
2021-01-23 14:04:06 +0100

    start implementation counting technique

--- data_association/counting_technique.py
+++ data_association/counting_technique.py
@@ -3,10 +3,14 @@
 """
 
 
-def associate_tracks(tracks_1, tracks_2, association_distance_threshold=10, consecutive_hits_confirm_association=3,
+def associate_tracks(tracks_1, tracks_2, previously_associated,
+                     association_distance_threshold=10,
+                     consecutive_hits_confirm_association=3,
                      consecutive_misses_end_association=2):
     """
 
     """
 
-    pass
+    if previously_associated:
+        # calculate distance between last state of tracks_1 and tracks_2
+        pass
