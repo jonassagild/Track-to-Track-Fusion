1104e3bf7a7dab9ab0939bf05a7e8a6f32992257
Jonas Åsnes  Sagild <jonassagild@hotmail.com>
2021-02-03 09:25:03 +0100

    add implementation of Bar-Shalom independence hypothesis test

---
+++ data_association/bar_shalom_test_associators.py
@@ -0,0 +1,43 @@
+"""
+contains two classes for the bar-shalom tests. One assuming independence, and one taking the dependence into account
+"""
+from data_association.Associator import Associator
+from data_association.track_to_track_association import test_association_dependent_tracks, \
+    test_association_independent_tracks
+
+
+class HypothesisTestIndependenceAssociator(Associator):
+    """
+
+    """
+
+    def __init__(self):
+        """
+
+        """
+        super().__init__()
+
+    def associate_tracks(self, tracks1, tracks2, **kwargs):
+        """
+        Performs an hypothesis test to check for association
+        """
+        return test_association_independent_tracks(tracks1[-1], tracks2[-1])
+
+
+class HypothesisTestDependenceAssociator(Associator):
+    """
+
+    """
+
+    def __init__(self):
+        """
+
+        """
+        super().__init__()
+
+    def associate_tracks(self, tracks1, tracks2, **kwargs):
+        """
+        Performs an hypothesis test to check for association
+        """
+
+        pass
